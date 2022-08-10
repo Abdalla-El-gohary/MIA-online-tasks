72a8b0c736fcb66777fe84cf17a98f4e543ae14a
Abdalla-El-gohary <110993994+Abdalla-El-gohary@users.noreply.github.com>
2022-08-10 17:23:56 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,61 +1,58 @@
 #include <iostream>
-int output[200];
-int count1=0;
+#include <string>
+#include <string.h>
+#include <cstdlib>
 using namespace std;
-void test(int n, int*h){
-    int seen, t,test=1;
-    while(test<n){
-            seen=1;
-            t=*(h+test);
-            if(*(h+test-1)<*(h+test)){
-                seen=1;
-            }
-            else{
-                for(int i=test;i<n-1;i++){
-                    if(*(h+i+1)>t){
-                    seen++;
-                     t=*(h+i+1);
-                }
-            }
-    }
-    output[count1]=seen;
-   // cout<<seen;
-    count1++;
-    test++;
-    }
-    output[count1]=0;
-    count1++;
+char output[1000];
+int count1=0;
 
+void encrypt(string input){
+    int n = input.length();
+    char arr[n+1];
+    strcpy(arr,input.c_str());
+    int r;
+    for(int i=0;i<n;i++){
+           if(arr[i+2]=='&'){
+                if(arr[i]=='1'){
+                    r=int(arr[i+1])-48;
+                    output[count1]= 'j'+r;
+                    count1++;
+                }
+                else if(arr[i]=='2'){
+                    r=int(arr[i+1])-38;
+                    output[count1]= 'j'+r;
+                    count1++;
+                }
+                i+=2;
+        }
+        else{
+            r=int(arr[i])-49;
+            output[count1]= 'a'+r;
+            count1++;
+        }
+    }
+    output[count1]='\n';
+    count1++;
 }
 int main()
 {
-    int q,n;
-    int heights[100];
+    int q;
+    string input;
     cin >>q;
     while(q>30 || q<0){
         cout << "invalid input,Enter a number from(1:30)";
         cin >>q;
     }
     while(q--){
-        cin >> n;
-        for(int i=0;i<n;i++){
-            cin >> heights[i];
-            for(int j=0;j<i;j++){
-                if (heights[j]==heights[i]){
-                    cout<< "duplicated number"<<endl;
-                    cin>>heights[i];
-                    break;
-                }
-            }
-        }
-        test(n,heights);
+        cin>> input;
+        encrypt(input);
     }
-    for(int a =0;a<count1;a++){
-        if(output[a]==0){
-            cout<<0<<endl;
+    for(int i=0;i<count1;i++){
+        if(output[i]=='\n'){
+            cout<<endl;
         }
         else{
-            cout<<output[a];
+            cout<<output[i];
         }
     }
     return 0;
